281e4f45acc22ecbc99f944d4293150cf472113b
Quraeshi99 <Quraeshi99@gmail.com>
2026-08-09 02:03:42 +0530

    Fix Composition import package

--- app/src/main/java/com/example/videocompressor/MediaCompressor.kt
+++ app/src/main/java/com/example/videocompressor/MediaCompressor.kt
@@ -5,7 +5,7 @@ import android.net.Uri
 import android.util.Log
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
-import androidx.media3.common.Composition
+import androidx.media3.transformer.Composition
 import androidx.media3.effect.Presentation
 import androidx.media3.transformer.DefaultEncoderFactory
 import androidx.media3.transformer.EditedMediaItem
